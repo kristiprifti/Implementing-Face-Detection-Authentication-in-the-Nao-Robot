1400e55e87248503c04fb0968d946eba8efdcf89
kristiprifti <61074990+kristiprifti@users.noreply.github.com>
2020-02-15 12:44:55 -0500

    Add files via upload

---
+++ TakeUsingNao.py
@@ -0,0 +1,69 @@
+import qi
+import argparse
+import sys
+import time
+from PIL import Image
+
+
+
+def main(session):
+    """
+    First get an image, then show it on the screen with PIL.
+    """
+    # Get the service ALVideoDevice.
+
+    video_service = session.service("ALVideoDevice")
+    resolution = 2    # VGA
+    colorSpace = 11   # RGB
+
+    videoClient = video_service.subscribe("python_client", resolution, colorSpace, 5)
+
+    t0 = time.time()
+
+    # Get a camera image.
+    # image[6] contains the image data passed as an array of ASCII chars.
+    naoImage = video_service.getImageRemote(videoClient)
+
+    t1 = time.time()
+
+    # Time the image transfer.
+    print "acquisition delay ", t1 - t0
+
+    video_service.unsubscribe(videoClient)
+
+
+    # Now we work with the image returned and save it as a PNG  using ImageDraw
+    # package.
+
+    # Get the image size and pixel array.
+    imageWidth = naoImage[0]
+    imageHeight = naoImage[1]
+    array = naoImage[6]
+    image_string = str(bytearray(array))
+
+    # Create a PIL Image from our pixel array.
+
+#def imageNo2d(self): 
+    im = Image.frombytes("RGB", (imageWidth, imageHeight), image_string)# Save the image.
+ #   image_name_2d = "images/img2d-" + str(self.imageNo2d) + ".png"
+    im.save("test1.png", "png") # Stored in images folder in the pwd, if not present then create one
+  #  self.imageNo2d += 1
+    im.show()
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--ip", type=str, default="192.168.1.144",
+                        help="Robot IP address. On robot or Local Naoqi: use '192.168.1.144'.")
+    parser.add_argument("--port", type=int, default=9559,
+                        help="Naoqi port number")
+
+    args = parser.parse_args()
+    session = qi.Session()
+    try:
+        session.connect("tcp://" + args.ip + ":" + str(args.port))
+    except RuntimeError:
+        print ("Can't connect to Naoqi at ip \"" + args.ip + "\" on port " + str(args.port) +".\n"
+               "Please check your script arguments. Run with -h option for help.")
+        sys.exit(1)
+    main(session)
