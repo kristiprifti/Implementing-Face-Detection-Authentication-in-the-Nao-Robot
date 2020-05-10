6d090424cf60146793dbb4b54dfa4bbc1fbbd489
kristiprifti <61074990+kristiprifti@users.noreply.github.com>
2020-05-10 03:54:17 -0400

    Delete Modifys_the_start_attedance_time.py

--- Modifys_the_start_attedance_time.py
+++
@@ -1,8 +0,0 @@
-dateandtime = input("Please enter date and time in this format  ex:(04/11/2020 02:06AM) : \n")   # asks user to input the date and time in 04/11/2020 02:06AM format
-courseID = input("Please enter courseID ex:(csc212):  \n")       # asks user to input name of the class
-with open('test2.txt','w+') as f:          # opens a text file. If it does'nt exists, it will create one
-
-
-	f.write(dateandtime )
-	f.write(courseID )            #save the information in the text file.
-	
