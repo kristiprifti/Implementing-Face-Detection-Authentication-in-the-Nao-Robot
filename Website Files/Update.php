157e34d6ef8886643cfaf226781248a223dce8ab
huedo100 <61330235+huedo100@users.noreply.github.com>
2020-05-09 23:43:05 -0400

    Update Update.php

--- Website Files/Update.php
+++ Website Files/Update.php
@@ -1,3 +1,4 @@
+
 <?php
 
 $servername = "localhost";
@@ -5,20 +6,19 @@ $username = "id12730815_rbproject";
 $password = "myproject";
 $database = "id12730815_attendancedb";
 
-
-// Create connection
+//Connect to the database to start working with databse
 $conn = mysqli_connect($servername, $username, $password,$database);
 
-//Check connection
+/*Check connection, if it can't connect to database so print out "Connection failed"; 
+otherwise, it will print "Connected sucessfully and we can start working with database */
 if (!$conn) {
     die("Connection failed: " . mysqli_connect_error());
 }
-
 echo "Connected successfully<br>"; 
 
 mysqli_select_db($conn,$database);
 
-// This is important to prevent injection attacks
+// Get the value from each box, this is important to prevent injection attacks
 $courseQuery = $_POST['Course'];
 $firstnameQuery = $_POST['FirstName'];
 $lastnameQuery = $_POST['LastName'];
@@ -26,20 +26,21 @@ $dateQuery = $_POST['Date'];
 $attendanceQuery = $_POST['Attendance'];
 $lateQuery = $_POST['Late'];
 
-//set attendance value default is 0, if it's on, it's 1
+//set attendance value default is 0, if checkbox is checked, its value is 1
 $att = 0;
 if($attendanceQuery == 'on'):
     $att = 1;
 endif;
 echo $att . "Attendance <br>";
-//set late value default is 0, if it's on, it's 1
+//set late value default is 0, if checkbox is checked, its value is 1
 $late = 0;
 if($lateQuery == 'on'):
     $late = 1;
 endif;
 echo $late . "Late <br>";
 
-//Insert data to database table with on duplicate ket update statement to avoid duplicate value
+/*Insert data to database table with on duplicate ket update statement to avoid duplicate value, 
+ course, firstname, lastname, date are set to primary key when create a dabase table*/
     $sql = "INSERT INTO Students(Course,  FirstName, LastName,Date, Attendance, Late) VALUES ('" . $courseQuery . "', '" . $firstnameQuery . "', '" . $lastnameQuery . "','" . $dateQuery . "',  '" . $att . "', '" . $late . "')
       ON DUPLICATE KEY UPDATE  Attendance='" . $att . "', Late ='" . $late . "'" ;
 
@@ -49,6 +50,6 @@ echo "New record created successfully<br>";
 echo "Error: " . $sql . "<br>" . mysqli_error($conn);
 }
 
-// Close the connection to the database server
+// Close the connection to the database server after finish working with database
 mysqli_close($conn);
 ?>
